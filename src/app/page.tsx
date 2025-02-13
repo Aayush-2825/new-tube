b8ff843a41fa17e8b5e559740989f6ce94e0df03
Aayush <aayush.ardy@gmail.com>
2025-02-13 17:29:07 +0530

    update

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return ( 
-    <div>
-      <Image src="/youtube.svg" width={50} height={50} alt="logo"/>
-      <p className="text-xl font-semibold tracking-tight">NewTube</p>
-    </div>   );
-}
---
+++ src/modules/home/ui/components/home-navbar/search-input.tsx
@@ -0,0 +1,16 @@
+import { SearchIcon } from "lucide-react"
+
+export const SearchInput = () =>{
+    return(
+    <form className="flex w-full max-w-[600px]">
+        <div className="relative w-full">
+            <input type="text" placeholder="text" className="w-full pl-4 py-2 pr-12 rounded-l-full border focus:outline-none focus:border-blue-500"></input> 
+        </div>
+        <div>
+            <button type="submit" className="px-5 py-2.5  bg-gray-100 border border-l-0 rounded-r-full hover:bg-gray-200 disabled:cursor-not-allowed disabled:opacity-50">
+                <SearchIcon className="size-5"/>
+            </button>
+        </div>
+    </form>
+    )
+}
